6f51a9c53de0504a6ef9bad95022e985aeb10d2c
sceptreCnsn <cansin.memis@clb.lcl>
2019-01-08 18:25:00 +0300

    commit

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React from "react";
+import { connect } from "react-redux";
+const StreamEdit = props => {
+  return <div>{props.stream.title}</div>;
+};
 
-const StreamEdit= () => {
-    return <div>StreamEdit</div>;
-}
+const mapStateToProps = (state, { match }) => {
+  return { stream: state.streams[match.params.id] };
+};
 
-export default StreamEdit;
+export default connect(mapStateToProps)(StreamEdit);
